d8be19ae6d418da3d54438dddb5e330232382ec6
kunalk444 <kanjwanikunal@gmail.com>
2026-01-29 07:14:58 +0530

    worked on filterbox,search

--- server/routes/problems.js
+++ server/routes/problems.js
@@ -1,26 +1,71 @@
 import { Router } from "express";
 import problemModel from "../models/problemModel.js";
 import { getProblemData } from "../controllers/insideproblem.js";
+import userModel from "../models/userModel.js";
+import { authMiddleware } from "../services/middleware.js";
+import { verifyToken } from "../services/jwttoken.js";
 
 const problemRouter = Router();
 
-problemRouter.get("/",async(req,res)=>{
-    const problems = await problemModel.find({},{
-        _id:1,
-        title:1,
-        serial_no:1,
-        difficulty:1,
-    });
-    if(problems)return res.json({success:true,problems});
-    return res.json({success:false,msg:"problem in fetching,try again later!"});
+problemRouter.get("/", async (req, res) => {
+    try {
+        const tag = req.query.tag;
+        const status = req.query.status;
+
+        const obj = {};
+
+        if (tag && tag !== "all") obj.difficulty = tag;
+        if (status && status !== "all") {
+
+            const token = req.cookies.jwt;
+            const verify = verifyToken(token);
+            if (verify.success) {
+                const userId = verify.userObj?.id;
+                const solvedByUser = await userModel.findById(userId, {
+                    solved: 1,
+                    _id: 0,
+                });
+
+                if (status === "solvedbyme") {
+                    obj._id = { $in: solvedByUser.solved }
+                } else {
+                    obj._id = { $nin: solvedByUser.solved }
+                }
+            }
+        }
+
+        const problems = await problemModel.find(obj, {
+            _id: 1,
+            title: 1,
+            serial_no: 1,
+            difficulty: 1,
+        });
+        if (problems) return res.json({ success: true, problems });
+        problemModel.fin
+        return res.json({ success: false, msg: "problem in fetching,try again later!" });
+    } catch (err) {
+        return res.json({ success: false, msg: err });
+    }
 
 });
 
-problemRouter.get("/insideproblem",async(req,res)=>{
+problemRouter.get("/insideproblem", async (req, res) => {
     const id = req.query.id;
     const ans = await getProblemData(id);
-    if(ans)return res.status(200).json({success:true,data:ans});
-    return res.status(404).json({success:false,msg:"Couldnt get Problem Data"});
+    if (ans) return res.status(200).json({ success: true, data: ans });
+    return res.status(404).json({ success: false, msg: "Couldnt get Problem Data" });
+});
+
+problemRouter.get("/search",async(req,res)=>{
+    const str = req.query.s;
+    const ans = await problemModel.find({
+        title:{$regex:str,$options:"i"},
+    },{
+        _id:1,
+        title:1,
+    });
+    if(!ans)return res.json({success:false});
+    return res.json({success:true,data:ans});
 });
 
 export default problemRouter;
--- server/routes/user.js
+++ server/routes/user.js
@@ -5,9 +5,9 @@ const userRouter = Router();
 
 userRouter.post("/progress",async(req,res)=>{
     const {success,type,msg,problemId} = req.body;
-    console.log(success,type,msg,problemId);
+    //console.log(success,type,msg,problemId);
     const user = req.user;
-    console.log(user)
+   // console.log(user)
     const ans = await handleUserProgress({success,type,msg,problemId,userId:user.id});
     return res.status(200).json({success:ans});
 });
